3c4b4c8dcadb3c77f85120c050a6de02f83497b2
ayoeze191 <olabodeezekiel2018@yahoo.com>
2022-04-25 16:00:48 +0100

    'changes'

--- src/Components/MultiChart.js
+++ src/Components/MultiChart.js
@@ -7,7 +7,7 @@ const MultiChart = () => {
         <div className='flex flex-col flex-1  items-center gap-5 md:pt-14 w-4/5 md:w-1/2 mx-auto text-center'>
           <h1 className='text-5xl font-bold text-silver-sand flex flex-col text-center'>MultiChart</h1>
         <h3 className='text-white text-lg'>Keep Your coins in the same place</h3>
-        <h3 className='text-white text-lg'>You can Generate Up to six charts and trackreal-timecurrency prices in addition to your price history</h3>
+        <h3 className='text-white text-lg'>You can Generate Up to six charts and track real-timecurrency prices in addition to your price history</h3>
         <div className='flex items-center gap-4 w-fit flex-col sm:flex-row'>
                 <input type='text' placeholder='Paste Your Eth Address Here' className='px-5 py-2 border-0 w-64 sm:w-96 text-black'/>
                 <button className='flex text-lg items-center px-7 py-2 bg-d rounded-lg text-white gap-2'><FaSearch fontSize='1.25rem' color='white'/>Go</button>
